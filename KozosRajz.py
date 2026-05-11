1cac5950f27c520b17b5570aa623e966fc836abd
0M1lan0 <ignacz.milan@diak.szamalk-szalezi.hu>
2026-05-11 08:44:19 +0200

    lampatest teteje

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,3 +1,23 @@
 #lámpatest
+import turtle
+
+turtle.width(3)
+
+hossz=100
+szog=90
+
+
+i=0
+
+while i<2:
+    turtle.forward(60)
+    turtle.left(szog)
+    turtle.forward(210)
+    turtle.left(szog)
+    turtle.forward(60)
+    i += 1
+
+
 
 #lampa korok es hatter
+turtle.done()
